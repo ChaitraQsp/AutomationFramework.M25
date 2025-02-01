38c28f1465477e669815264ca6a4d75ab7358eb7
ChaitraQsp <chaitraqspvirtual@gmail.com>
2025-02-01 11:16:26 +0530

    Demo Committed

--- src/test/java/practice/AddProductToCartAndValidate.java
+++ src/test/java/practice/AddProductToCartAndValidate.java
@@ -20,6 +20,7 @@ public class AddProductToCartAndValidate {
 		driver.get("https://www.saucedemo.com/");
 		
 		//Step 2: Login to Application
+		//Step 2: Login to Application
 		driver.findElement(By.id("user-name")).sendKeys("standard_user");
 		driver.findElement(By.id("password")).sendKeys("secret_sauce");
 		driver.findElement(By.id("login-button")).click();
